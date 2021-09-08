4bea94c63d246a266123c185af5e60c0f421b553
sahuadarsh0 <sahuadarsh0@gmail.com>
2021-09-08 17:01:54 +0530

    Added Payment Details

--- app/src/main/java/sdr/tecqza/homedelivery_deliveryboy/ui/payment/PaymentAdapter.kt
+++ app/src/main/java/sdr/tecqza/homedelivery_deliveryboy/ui/payment/PaymentAdapter.kt
@@ -1,9 +1,18 @@
 package sdr.tecqza.homedelivery_deliveryboy.ui.payment
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.customview.customView
+import com.afollestad.materialdialogs.customview.getCustomView
+import com.afollestad.materialdialogs.list.customListAdapter
+import sdr.tecqza.homedelivery_deliveryboy.R
 import sdr.tecqza.homedelivery_deliveryboy.databinding.ListPaymentBinding
+import sdr.tecqza.homedelivery_deliveryboy.databinding.PaymentDetailsBinding
 import sdr.tecqza.homedelivery_deliveryboy.model.Payment
 import technited.minds.androidutils.ProcessDialog
 
@@ -40,11 +49,54 @@ class PaymentAdapter : RecyclerView.Adapter<PaymentAdapter.PaymentViewHolder>() 
     }
 
     inner class PaymentViewHolder(private val binding: ListPaymentBinding) : RecyclerView.ViewHolder(binding.root) {
+
         fun bind(payment: Payment) {
             binding.apply {
                 month.text = payment.month
                 netSalary.text = "Rs. ${payment.netSalary}"
+                card.setOnClickListener {
+                    openDialog(payment)
+                }
+                monthCard.setOnClickListener {
+                    openDialog(payment)
+                }
             }
+        }
+
+        private fun openDialog(payment: Payment) {
+            Log.d("asa", "openDialog:$payment")
+            val dialog = MaterialDialog(itemView.context).show {
+                title(text = payment.month)
+                cornerRadius(16f)
+                customView(R.layout.payment_details, scrollable = true, horizontalPadding = true)
+                positiveButton(text = "OK") { dialog ->
+                    dialog.dismiss()
+                }
+            }
+            val customView = dialog.getCustomView()
+            val da: TextView = customView.findViewById(R.id.da)
+            val hra: TextView = customView.findViewById(R.id.hra)
+            val lta: TextView = customView.findViewById(R.id.lta)
+            val bonus: TextView = customView.findViewById(R.id.bonus)
+            val specialAllowance: TextView = customView.findViewById(R.id.special_allowance)
+            val otherAllowance: TextView = customView.findViewById(R.id.other_allowance)
+            val pf: TextView = customView.findViewById(R.id.pf)
+            val esi: TextView = customView.findViewById(R.id.esi)
+            val other: TextView = customView.findViewById(R.id.other)
+            val basicPay: TextView = customView.findViewById(R.id.basic_pay)
+            val netSalary: TextView = customView.findViewById(R.id.net_salary)
+
+            da.text = payment.da
+            hra.text = payment.hra
+            lta.text = payment.lta
+            bonus.text = payment.bonus
+            specialAllowance.text = payment.specialAllowance
+            otherAllowance.text = payment.otherAllowance
+            pf.text = payment.pf
+            esi.text = payment.esi
+            other.text = payment.other
+            basicPay.text = payment.basicPay
+            netSalary.text = payment.netSalary
         }
 
     }
--- app/src/main/java/sdr/tecqza/homedelivery_deliveryboy/ui/payment/PaymentHistoryFragment.kt
+++ app/src/main/java/sdr/tecqza/homedelivery_deliveryboy/ui/payment/PaymentHistoryFragment.kt
@@ -1,40 +1,20 @@
 package sdr.tecqza.homedelivery_deliveryboy.ui.payment
 
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.afollestad.materialdialogs.MaterialDialog
-import com.afollestad.materialdialogs.datetime.datePicker
-import com.afollestad.materialdialogs.datetime.selectedDate
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import sdr.tecqza.homedelivery_deliveryboy.R
 import sdr.tecqza.homedelivery_deliveryboy.api.RiderService
-import sdr.tecqza.homedelivery_deliveryboy.databinding.FragmentOrderHistoryBinding
 import sdr.tecqza.homedelivery_deliveryboy.databinding.FragmentPaymentHistoryBinding
-import sdr.tecqza.homedelivery_deliveryboy.model.Order
 import sdr.tecqza.homedelivery_deliveryboy.model.Payment
-import sdr.tecqza.homedelivery_deliveryboy.ui.order.OrderAdapter
-import sdr.tecqza.homedelivery_deliveryboy.ui.order.OrderHistoryFragmentArgs
-import sdr.tecqza.homedelivery_deliveryboy.ui.order.OrderViewModel
 import technited.minds.androidutils.ProcessDialog
 import technited.minds.androidutils.SharedPrefs
-import java.text.SimpleDateFormat
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-import java.time.temporal.TemporalAdjusters
-import java.util.*
 
 class PaymentHistoryFragment : Fragment() {
 
@@ -43,8 +23,6 @@ class PaymentHistoryFragment : Fragment() {
     private lateinit var userSharedPreferences: SharedPrefs
     private lateinit var processDialog: ProcessDialog
     private var payments: ArrayList<Payment>? = null
-    private var startCalender: LocalDate? = null
-    private var endCalender: LocalDate? = null
     private val adapter = PaymentAdapter()
 
     override fun onCreateView(
@@ -59,35 +37,21 @@ class PaymentHistoryFragment : Fragment() {
         processDialog = ProcessDialog(requireContext())
         userSharedPreferences = SharedPrefs(requireContext(), "USER")
 
-        binding.paymentsList.layoutManager = GridLayoutManager(context,2)
+        binding.paymentsList.layoutManager = GridLayoutManager(context, 2)
         binding.paymentsList.adapter = adapter
         processDialog.show()
 
         getAllPayment()
 
-         binding.advanced.setOnClickListener {
+        binding.advanced.setOnClickListener {
             startC()
         }
         return root
     }
 
     private fun startC() {
-        MaterialDialog(requireContext()).show {
-            title(text = "Select Start Date")
-            cornerRadius(16f)
-            datePicker(maxDate = Calendar.getInstance()) { dialog, date ->
-                Toast.makeText(context, ("Selected Start date: ${selectedDate().formatDate()}"), Toast.LENGTH_SHORT).show()
-                dialog.setOnDismissListener {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                        startCalender = LocalDate.parse(selectedDate().formatDate())
-                    } else Toast.makeText(context, "Not Supported", Toast.LENGTH_SHORT).show()
-
-                }
-            }
-        }
     }
 
- 
     private fun getAllPayment() {
         val order = RiderService.create().payment("21")
 //        val order = RiderService.create().payment(userSharedPreferences["riderId"])
@@ -111,81 +75,6 @@ class PaymentHistoryFragment : Fragment() {
                 processDialog.dismiss()
             }
         })
-    }
-
-
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun filter(type: String) {
-        val filteredList = arrayListOf<Payment>()
-        val Today = LocalDate.now()
-        val Formatter = DateTimeFormatter.ofPattern("dd/MMM/yyyy")
-      when (type) {
-            "Today" -> {
-                val currentDate = Today.format(Formatter)
-                payments?.let {
-                    for (item in it) {
-                        if (item.date!!.contains(currentDate)) {
-                            filteredList.add(item)
-                        }
-                    }
-                }
-            }
-
-            "Yesterday" -> {
-                val yesterday0 = Today.minusDays(1)
-                Log.d("asa", "filter: yesterday $yesterday0")
-                val yesterday = yesterday0.format(Formatter)
-                Log.d("asa", "filter: formattedYesterday $yesterday")
-                payments?.let {
-                    for (item in it) {
-                        if (item.date!!.contains(yesterday.toString())) {
-                            filteredList.add(item)
-                        }
-                    }
-                }
-            }
-            "Mtd" -> {
-
-                val first = Today.with(TemporalAdjusters.firstDayOfMonth())
-                Log.d("asa", "filter: first $first")
-                val firstDayOfMonth = first.format(Formatter)
-                Log.d("asa", "filter: firstDayOfMonth $firstDayOfMonth")
-                val ranges = first..Today
-                payments?.let {
-                    for (item in it) {
-                        if (LocalDate.parse(item.date!!, DateTimeFormatter.ofPattern("dd/MMM/yyyy")) in ranges) {
-                            filteredList.add(item)
-                        }
-                    }
-                }
-            }
-            "Range" -> {
-
-                val first = startCalender
-                val last = endCalender
-                Log.d("asa", "filter: first $first")
-                Log.d("asa", "filter: last $last")
-                val firstDay = first?.format(Formatter)
-                val lastDay = last?.format(Formatter)
-                Log.d("asa", "filter: firstDay $firstDay")
-                Log.d("asa", "filter: lastDay $lastDay")
-                val ranges = first!!..last!!
-                payments?.let {
-                    for (item in it) {
-                        if (LocalDate.parse(item.date!!, DateTimeFormatter.ofPattern("dd/MMM/yyyy")) in ranges) {
-                            filteredList.add(item)
-                        }
-                    }
-                }
-            }
-        }
-        adapter.setList(filteredList)
-        adapter.notifyDataSetChanged()
-
-    }
-
-    private fun Calendar.formatDate(): String {
-        return SimpleDateFormat("yyyy-MM-dd", Locale.US).format(this.time)
     }
 
     override fun onDestroyView() {
